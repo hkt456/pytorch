e56cf51287bcf98836327993796c89b2deb692ce
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-10-30 23:26:10 +0700

    added engine.py

---
+++ going_modular/engine.py
@@ -0,0 +1,111 @@
+import torch
+
+from tqdm.auto import tqdm
+from typing import Tuple, List, Dict
+
+def train_step(model: torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               device: torch.device) -> Tuple[float, float]:
+    """
+    Returns a tuple of (train_loss, train_accuracy).
+    """
+    model.train()
+
+    train_loss = 0.0, train_acc = 0.0
+
+    for batch, (X,y), in enumerate(dataloader):
+        X,y = X.to(device), y.to(device)
+
+        y_pred = model(X)
+
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+
+        optimizer.zero_grad()
+
+        loss.backward()
+
+        optimizer.step()
+
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += torch.sum(y_pred_class == y).item() / len(y)
+
+    return train_loss / len(dataloader), train_acc / len(dataloader)
+
+def test_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
+              loss_fn: torch.nn.Module,
+              device: torch.device) -> Tuple[float, float]:
+    """ 
+    Returns a tuple of (test_loss, test_accuracy).
+    """
+    model.eval()
+
+    test_loss = 0.0, test_acc = 0.0
+
+    with torch.inference_mode():
+        for batch, (X,y), in enumerate(dataloader):
+            X,y = X.to(device), y.to(device)
+
+            y_pred_logits = model(X)
+
+            loss = loss_fn(y_pred_logits, y)
+            test_loss += loss.item()
+
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += ((test_pred_labels == y).sum().item()/len(test_pred_labels))
+            
+    return test_loss / len(dataloader), test_acc / len(dataloader)
+
+def train(model: torch.nn.Module, 
+          train_dataloader: torch.utils.data.DataLoader, 
+          test_dataloader: torch.utils.data.DataLoader, 
+          optimizer: torch.optim.Optimizer,
+          loss_fn: torch.nn.Module,
+          epochs: int,
+          device: torch.device) -> Dict[str, List]:
+    """
+    A dictionary of training and testing loss as well as training and
+    testing accuracy metrics. Each metric has a value in a list for 
+    each epoch.
+    In the form: {train_loss: [...],
+                  train_acc: [...],
+                  test_loss: [...],
+                  test_acc: [...]} 
+    For example if training for epochs=2: 
+                 {train_loss: [2.0616, 1.0537],
+                  train_acc: [0.3945, 0.3945],
+                  test_loss: [1.2641, 1.5706],
+                  test_acc: [0.3400, 0.2973]}
+    """
+    results = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                          dataloader=train_dataloader,
+                                          loss_fn=loss_fn,
+                                          optimizer=optimizer,
+                                          device=device)
+        test_loss, test_acc = test_step(model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn,
+            device=device)
+
+      # Print out what's happening
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+      # Update results dictionary
+    results["train_loss"].append(train_loss)
+    results["train_acc"].append(train_acc)
+    results["test_loss"].append(test_loss)
+    results["test_acc"].append(test_acc)
+
+    return results
